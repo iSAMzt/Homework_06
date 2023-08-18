8b39aea7b45cc6944f4c0be183cd3d97d8ffcea6
iSAMzt <surskovalexey@gmail.com>
2023-08-18 11:56:13 +0300

    first
    first

---
+++ file_parser.py
@@ -0,0 +1,127 @@
+import sys
+from pathlib import Path
+
+JPEG_IMAGES = []
+JPG_IMAGES = []
+PNG_IMAGES = []
+SVG_IMAGES = []
+
+AVI_VIDEOS = []
+MP4_VIDEOS = []
+MOV_VIDEOS = []
+MKV_VIDEOS = []
+
+DOC_DOCUMENTS = []
+DOCX_DOCUMENTS = []
+TXT_DOCUMENTS = []
+PDF_DOCUMENTS = []
+XLSX_DOCUMENTS = []
+PPTX_DOCUMENTS = []
+
+MP3_AUDIO = []
+OGG_AUDIO = []
+WAV_AUDIO = []
+AMR_AUDIO = []
+
+
+MY_OTHER = []
+ARCHIVES = []
+
+REGISTER_EXTENSION = {
+    'JPEG': JPEG_IMAGES,
+    'JPG': JPG_IMAGES,
+    'PNG': PNG_IMAGES,
+    'SVG': SVG_IMAGES,
+
+    'AVI': AVI_VIDEOS,
+    'MP4': MP4_VIDEOS,
+    'MOV': MOV_VIDEOS,
+    'MKV': MKV_VIDEOS,
+
+    'DOC': DOC_DOCUMENTS,
+    'DOCX': DOCX_DOCUMENTS,
+    'TXT': TXT_DOCUMENTS,
+    'PDF': PDF_DOCUMENTS,
+    'XLSX': XLSX_DOCUMENTS,
+    'PPTX': PPTX_DOCUMENTS,
+
+    'MP3': MP3_AUDIO,
+    'OGG': OGG_AUDIO,
+    'WAV': WAV_AUDIO,
+    'AMR': AMR_AUDIO,
+
+    'ZIP': ARCHIVES,
+    'GZ': ARCHIVES,
+    'TAR': ARCHIVES
+}
+
+FOLDERS = []
+EXTENSION = set()
+UNKNOWN = set()
+
+
+def get_extension(filename: str) -> str:
+    # перетворюємо розширення файлу на назву папки jpg -> JPG
+    return Path(filename).suffix[1:].upper()
+
+
+def scan(folder: Path) -> None:
+    for item in folder.iterdir():
+        # Якщо це папка то додаємо її до списку FOLDERS і переходимо до наступного елемента папки
+        if item.is_dir():
+            # перевіряємо, щоб папка не була тією в яку ми складаємо вже файли
+            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'MY_OTHER'):
+                FOLDERS.append(item)
+                # скануємо вкладену папку
+                scan(item)  # рекурсія
+            continue  # переходимо до наступного елементу в сканованій папці
+
+        #  Робота з файлом
+        ext = get_extension(item.name)  # беремо розширення файлу
+        fullname = folder / item.name  # беремо шлях до файлу
+        if not ext:  # якщо файл немає розширення то додаєм до невідомих
+            MY_OTHER.append(fullname)
+        else:
+            try:
+                container = REGISTER_EXTENSION[ext]
+                EXTENSION.add(ext)
+                container.append(fullname)
+            except KeyError:
+                # Якщо ми не зареєстрували розширення у REGISTER_EXTENSION, то додаємо до невідомих
+                UNKNOWN.add(ext)
+                MY_OTHER.append(fullname)
+
+
+if __name__ == "__main__":
+    folder_to_scan = sys.argv[1]
+    print(f'Start in folder {folder_to_scan}')
+    scan(Path(folder_to_scan))
+    print(f'Images jpeg: {JPEG_IMAGES}')
+    print(f'Images jpg: {JPG_IMAGES}')
+    print(f'Images svg: {SVG_IMAGES}')
+    print(f'Images png: {PNG_IMAGES}')
+
+    print(f'Video avi: {AVI_VIDEOS}')
+    print(f'Video mp4: {MP4_VIDEOS}')
+    print(f'Video mov: {MOV_VIDEOS}')
+    print(f'Video mkv: {MKV_VIDEOS}')
+
+    print(f'Documents doc: {DOC_DOCUMENTS}')
+    print(f'Documents docx: {DOCX_DOCUMENTS}')
+    print(f'Documents txt: {TXT_DOCUMENTS}')
+    print(f'Documents pdf: {PDF_DOCUMENTS}')
+    print(f'Documents xlsx: {XLSX_DOCUMENTS}')
+    print(f'Documents pptx: {PPTX_DOCUMENTS}')
+
+    print(f'Audio mp3: {MP3_AUDIO}')
+    print(f'Audio ogg: {OGG_AUDIO}')
+    print(f'Audio wav: {WAV_AUDIO}')
+    print(f'Audio amr: {AMR_AUDIO}')
+
+    print(f'Archives: {ARCHIVES}')
+
+    print(f'Types of files in folder: {EXTENSION}')
+    print(f'Unknown files of types: {UNKNOWN}')
+    print(f'MY_OTHER: {MY_OTHER}')
+
+    print(FOLDERS)
---
+++ normalize.py
@@ -0,0 +1,21 @@
+import re
+
+CYRILLIC_SYMBOLS = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ'
+TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "u", "ja", "je", "ji", "g")
+TRANS = {}
+for c, l in zip(CYRILLIC_SYMBOLS, TRANSLATION):
+    TRANS[ord(c)] = l
+    TRANS[ord(c.upper())] = l.upper()
+
+
+def normalize(name: str) -> str:
+    t_name = name.translate(TRANS)
+    t_name = re.sub(r'\W', '_', t_name)
+    #t_name = re.sub(r"_$", ".", t_name)
+    #t_name = t_name.replace("_",".")
+    temp_str = t_name[::-1]
+    t_name = temp_str.replace("_",".",1)
+    t_name = t_name[::-1]
+    return t_name
+
